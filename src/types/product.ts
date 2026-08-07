196e1369f7d29f2ab9be2c167bdcac9ef19e5ae0
Jhon Bazw <bazwjhon@gmail.com>
2026-08-07 02:30:24 +0000

    Add new file

---
+++ src/types/product.ts
@@ -0,0 +1,30 @@
+export interface Store {
+  id: string
+  vendor_id: string
+  store_name: string
+  slug: string
+  description?: string
+  is_verified: boolean
+  created_at: string
+}
+
+export interface Category {
+  id: string
+  name: string
+  slug: string
+  parent_id?: string
+}
+
+export interface Product {
+  id: string
+  store_id: string
+  category_id?: string
+  title: string
+  slug: string
+  description?: string
+  price: number
+  stock: number
+  images: string[]
+  is_active: boolean
+  created_at: string
+}
