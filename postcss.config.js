dd1809b5a418c0f68d6921aa05788f53438fff33
Jhon Bazw <bazwjhon@gmail.com>
2026-08-08 05:04:22 +0000

    Edit postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,21 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  }
 };
+
+export default nextConfig;
+
+initOpenNextCloudflareForDev();
