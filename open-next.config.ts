b12c461b0f33a413eb6948ca2b9f7630d9c0e4a3
Jhon Bazw <bazwjhon@gmail.com>
2026-08-09 19:29:05 +0000

    Edit open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,24 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-  // Configuración estándar y compatible con OpenNext para Cloudflare
+defineCloudflareConfig({
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  edgeExternals: ["node:crypto"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
 });
