efc94054c4f54dde9a16f109d553d4fdf284aef4
Jhon Bazw <bazwjhon@gmail.com>
2026-08-09 21:38:40 +0000

    Edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,5 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+import type { NextConfig } from "next";
 
-export default defineCloudflareConfig({
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-  edgeExternals: ["node:crypto"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-});
+const nextConfig: NextConfig = {};
+
+export default nextConfig;
