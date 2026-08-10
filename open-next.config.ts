e364e3243be9cc14f6f394f5e3f41f96b561599e
Jhon Bazw <bazwjhon@gmail.com>
2026-08-10 03:36:14 +0000

    Edit open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,5 @@
+import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
+
 const config: OpenNextConfig = {
   default: {
     override: {
@@ -22,3 +24,5 @@ const config: OpenNextConfig = {
     },
   },
 };
+
+export default config;
