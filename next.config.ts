e65872a34ce72e5d7e90d068de2e62370c2fa358
Jhon Bazw <bazwjhon@gmail.com>
2026-08-11 14:01:49 +0000

    Edit next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    reactCompiler: true,
+    reactCompiler: true, // Activa oficialmente el compilador de React 19
   },
 };
 
